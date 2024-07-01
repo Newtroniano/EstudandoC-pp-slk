04b459175f04f2c8bef7e5e4092ada92e88a5989
Newtroniano <luisocara.luis@gmail.com>
2024-07-01 13:02:11 -0300

    chance

---
+++ ConsoleApplication1/Ponteiro.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication1.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	string veiculo = "Carro"
+	string * pv;
+
+	pv = veiculo;
+
+	count << veiculo;
+
+	return 0;
+
+
+}
+
